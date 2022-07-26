d97d7fae6e18bb62c9f75abdd68b5b64e91b0257
Rafael <89470801+raph-ostrovsky@users.noreply.github.com>
2022-07-26 17:32:05 +0200

    Create spoj_suma.cpp

---
+++ spoj_suma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    int sum = 0;
+    while (cin >> number)
+    {
+        sum += number;
+        cout << sum << endl;
+    }
+	
+
+	return 0;
+}
